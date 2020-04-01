5483335fe39467bd7f2202746973888876e93b8c
川口美咲 <kawaguchimisaki@kawaguchimisakinoMacBook-Pro.local>
2020-04-01 21:27:45 +0900

    [Modify]userとpostの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  # postが削除されるとuserも削除される
+  has_many :posts, dependent: :destroy
 end
