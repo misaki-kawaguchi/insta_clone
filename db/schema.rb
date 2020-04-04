ea23ac2eaf9edf84b36fcfa69ad205072bc8e8fd
川口美咲 <kawaguchimisaki@kawaguchimisakinoMacBook-Pro.local>
2020-04-04 22:39:42 +0900

    [Modify]usernameにもunique制約つける

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_150038) do
+ActiveRecord::Schema.define(version: 2020_04_04_133212) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_150038) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "posts", "users"
