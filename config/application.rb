6b2e60cf92e1d46bf43cd10696ae4fbb8f94ac1a
川口美咲 <kawaguchimisaki@kawaguchimisakinoMacBook-Pro.local>
2020-04-04 23:33:29 +0900

    [Modify]bundle exec rubocop -aで自動的に直らない箇所を手動で修正する

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,7 @@ module InstaClone
     config.active_record.default_timezone = :local
 
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s] を/ で区切るようにした
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
   end
 end
