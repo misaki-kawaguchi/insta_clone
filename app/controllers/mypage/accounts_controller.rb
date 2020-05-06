2412f97e3d0e81d4fac14c9d088c84b154469806
川口美咲 <misaki@arcphilia.co.jp>
2020-05-06 16:55:50 +0900

    [Add]プロフィール編集画面実装

--- app/controllers/mypage/accounts_controller.rb
+++ app/controllers/mypage/accounts_controller.rb
@@ -1,4 +1,26 @@
 class Mypage::AccountsController < Mypage::BaseController
+
+  # /mypage/account/edit
+  # 現在ログインしているユーザーのidを探す
   def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(account_params)
+      redirect_to edit_mypage_account_path, success: 'プロフィールを更新しました'
+    else
+      flash.now['danger'] = 'プロフィールの更新に失敗しました'
+      render :edit
+    end
+
+  end
+
+  private
+
+  # :avatar_cache：画像を変更していない場合は、前回アップロードした画像は消えない
+  def account_params
+    params.require(:user).permit(:email, :username, :avatar, :avatar_cache)
   end
 end
