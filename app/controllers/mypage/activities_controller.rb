2200b59871d9ad9d626fd7a8abc88c4678667d5b
川口美咲 <misaki@arcphilia.co.jp>
2020-05-12 23:57:05 +0900

    [Add]通知一覧ページ作成

--- app/controllers/mypage/activities_controller.rb
+++ app/controllers/mypage/activities_controller.rb
@@ -1,4 +1,9 @@
 class Mypage::ActivitiesController < Mypage::BaseController
+  before_action :require_login, only: %i[index]
+
+  # mypage/activities
+  # 通知リストに最新の10件を表示させる
   def index
+    @activities = current_user.activities.order(created_at: :desc).page(params[:page]).per(10)
   end
 end
