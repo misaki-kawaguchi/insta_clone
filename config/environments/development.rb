cd62d8e098ee82791d5c0891db1b2166f6771455
川口美咲 <misaki@arcphilia.co.jp>
2020-05-27 14:45:50 +0900

    [Add]コメント追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   config.session_store :redis_store, servers: 'redis://localhost:6379', expire_after: 1.day
 
   # デフォルトのURLを指定する
+  # config/settings/development.ymlでで設定したdefault_url_optionsをハッシュ化する{ :host => 'localhost:3000'}
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
   # config.action_mailer.delivery_methodは:letter_opener_webに変更する
   config.action_mailer.delivery_method = :letter_opener_web
