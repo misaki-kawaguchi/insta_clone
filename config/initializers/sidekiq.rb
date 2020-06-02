0d990873e568654040b0a956edd373a488020b5a
川口美咲 <misaki@arcphilia.co.jp>
2020-06-03 00:25:02 +0900

    [Add]initializersにSidekiqの設定を追加

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+# サーバー側の設定
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://localhost:6379' }
+end
+
+# クライアント側の設定
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://localhost:6379' }
+end
