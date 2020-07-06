66ebc97b2d13bffac958caab984074771ce80b6d
川口美咲 <misaki@arcphilia.co.jp>
2020-07-06 23:33:22 +0900

    [Add]postモデルのバリデーションについてのテストを追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # バリデーションについて
+  describe 'バリデーション' do
+    # validates :images, presence: true
+    it '画像は必須であること' do
+      post = build(:post, images: nil)
+      post.valid?
+      expect(post.errors[:images]).to include('を入力してください')
+    end
+
+    # validates :body, presence: true, length: { maximum: 500 }
+    it '本文は最大500文字であること' do
+      post = build(:post, body: "a" * 1001)
+      post.valid?
+      expect(post.errors[:body]).to include('は500文字以内で入力してください')
+    end
+  end
 end
