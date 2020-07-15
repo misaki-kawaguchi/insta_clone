2c461c099c75ffceab3a009ef0f683c5893ef257
川口美咲 <misaki@arcphilia.co.jp>
2020-07-15 22:51:29 +0900

    [Modify]ログアウトのシステムスペック追加し忘れ

--- spec/system/user_sessions/user_session_spec.rb
+++ spec/system/user_sessions/user_session_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'ログイン・ログアウト', type: :system do
         fill_in 'メールアドレス', with: user.email
         # パスワードを入力
         fill_in 'パスワード', with: '0123456789'
-        # ログインボタンを押す
+        # ログインボタンを押す（submitボタンやbuttonタグの場合）
         click_button 'ログイン'
         # 現在のページが特定のパス（投稿一覧ページ）であることを検証する
         expect(current_path).to eq root_path
@@ -33,13 +33,28 @@ RSpec.describe 'ログイン・ログアウト', type: :system do
         fill_in 'メールアドレス', with: user.email
         # パスワードを入力
         fill_in 'パスワード', with: '01234'
-        # ログインボタンを押す
+        # ログインボタンを押す（submitボタンやbuttonタグの場合）
         click_button 'ログイン'
         # 現在のページが特定のパス（ログインページ）であることを検証する
         expect(current_path).to eq login_path
         # ログインに失敗したことを検証する（page内にログインに失敗しましたというテキストが存在するかどうかを確認する）
         expect(page).to have_content 'ログインに失敗しました'
       end
+    end
+  end
+  # ログアウトについて
+  describe 'ログアウト' do
+    # ログインする（テストの実行前に毎回実行される）
+    before do
+      login
+    end
+    it 'ログアウトできること' do
+      # ログアウトボタンを押す
+      click_on('ログアウト')
+      # 現在のページが特定のパス（投稿一覧ページ）であることを検証する
+      expect(current_path).to eq root_path
+      # ログアウトに成功したことを検証する（page内にログアウトしましたというテキストが存在するかどうかを確認する）
+      expect(page).to have_content 'ログアウトしました'
     end
   end
 end
